39b275c19d54da4ff96e7b58bdf272927c876c62
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-02-07 23:52:22 -0300

    feat: population

--- obj/Debug/net8.0/TaskApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fafe0131c0642a292fc8eff483014ed7e2e542c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
